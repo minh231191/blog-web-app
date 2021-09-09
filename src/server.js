04f44e565f943e08f577510c43a283867e199a09
Minh Hoang <minh231191@gmail.com>
2021-09-09 23:24:39 +0700

    Update server.js cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const whitelist = ['https://minhhn-blog-be.herokuapp.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+      if (!origin) {
+          return callback(null, true);
+      }
+
+      if (whitelist.indexOf(origin) === -1) {
+          var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  }
+}
+
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/minhhn-blog-web-app'));
